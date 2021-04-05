a45d29998be426afb2e47c8a9f40787e9a933a82
Lyapk <Elena525658>
2021-04-05 15:58:31 +0300

    calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("qwe");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите 1 значение: ");
+        Double a = in.nextDouble();
+        System.out.print("Введите 2 значение: ");
+        Double b = in.nextDouble();
+        System.out.println("Введите оператор: ");
+        String operator = in.next();
+        System.out.printf("%.4f", calculator(a, b, operator));
+    }
+
+    /**
+     * Метод калькулятора
+     */
+    private static double calculator(Double a, Double b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                System.out.println("Невалидный оператор!");
+                return -100;
+        }
     }
 }
